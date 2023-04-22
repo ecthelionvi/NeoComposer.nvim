d2dddf198ff0dc16b741a65614b9516232b61d34
Robert Sears <rob@MacBook-Air-10.home>
2023-04-22 07:58:55 -0500

    Update Setup

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,3 +1,11 @@
-if not vim.g.neocomposer_setup then
-  require('NeoComposer').setup()
+-- if not vim.g.neocomposer_setup then
+-- require('NeoComposer').setup()
+-- end
+
+if vim.g.neocmoposer_setup then
+  return
 end
+
+require('NeoComposer').setup()
+
+vim.g.neocmoposer_setup = true
